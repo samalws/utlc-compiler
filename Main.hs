50dae64b619c03518d59a77d63fd2f4e8e141928
Sam Alws <sam@samalws.com>
2021-09-28 17:18:58 -0400

    boolean algebra; fast nats that can run most operations in log(n)

--- Main.hs
+++ Main.hs
@@ -5,9 +5,9 @@ import Text.Parsec
 import Data.Either
 import System.Process
 
-typePostfix = " | PI Int | PSucc_0"
-evalPostfix = "\n" <> "veval PSucc_0 (PI n) = (PI (n+1))" <> "\n" <> "dePI (PI n) = n"
-mainFn = "main = print $ dePI $ veval (veval Cmain_0 PSucc_0) (PI 0)"
+typePostfix = " | PI Int | PSucc_0 | PAdd_0 | PAdd_1 Monotype | PMul_0 | PMul_1 Monotype"
+evalPostfix = "\nveval PSucc_0 (PI n) = (PI (n+1)) \nveval PAdd_0 x = PAdd_1 x \nveval (PAdd_1 (PI x)) (PI y) = PI (x + y) \nveval PMul_0 x = PMul_1 x \nveval (PMul_1 (PI x)) (PI y) = PI (x*y) \ndePI (PI n) = n"
+mainFn = "main = print $ dePI $ veval (veval (veval (veval Cmain_0 (PI 0)) PSucc_0) PAdd_0) PMul_0"
 
 main = do
   text <- readFile "main.utlc"
