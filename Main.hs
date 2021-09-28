3c471d8cf545d0e3a4e12b9558927b332622a7af
Sam Alws <sam@samalws.com>
2021-09-28 17:32:18 -0400

    move code additions into separate files

--- Main.hs
+++ Main.hs
@@ -5,11 +5,11 @@ import Text.Parsec
 import Data.Either
 import System.Process
 
-typePostfix = " | PI Integer | PSucc_0 | PAdd_0 | PAdd_1 Monotype | PMul_0 | PMul_1 Monotype"
-evalPostfix = "\nveval PSucc_0 (PI n) = (PI (n+1)) \nveval PAdd_0 x = PAdd_1 x \nveval (PAdd_1 (PI x)) (PI y) = PI (x + y) \nveval PMul_0 x = PMul_1 x \nveval (PMul_1 (PI x)) (PI y) = PI (x*y) \ndePI (PI n) = n"
-mainFn = "main = print $ dePI $ veval (veval (veval (veval Cmain_0 (PI 0)) PSucc_0) PAdd_0) PMul_0"
-
 main = do
+  typePostfix <- readFile "CodeAdditions/TypePostfix"
+  evalPostfix <- readFile "CodeAdditions/EvalPostfix"
+  mainFn      <- readFile "CodeAdditions/MainFn"
+
   text <- readFile "main.utlc"
   putStrLn text
   let parsed = parse codeFileParser "main.utlc" text
