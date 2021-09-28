ff92cb149c6b40b62d5378eeb1349c616b9f51e6
Sam Alws <sam@samalws.com>
2021-09-28 17:25:43 -0400

    arbitrary precision ints

--- Main.hs
+++ Main.hs
@@ -5,7 +5,7 @@ import Text.Parsec
 import Data.Either
 import System.Process
 
-typePostfix = " | PI Int | PSucc_0 | PAdd_0 | PAdd_1 Monotype | PMul_0 | PMul_1 Monotype"
+typePostfix = " | PI Integer | PSucc_0 | PAdd_0 | PAdd_1 Monotype | PMul_0 | PMul_1 Monotype"
 evalPostfix = "\nveval PSucc_0 (PI n) = (PI (n+1)) \nveval PAdd_0 x = PAdd_1 x \nveval (PAdd_1 (PI x)) (PI y) = PI (x + y) \nveval PMul_0 x = PMul_1 x \nveval (PMul_1 (PI x)) (PI y) = PI (x*y) \ndePI (PI n) = n"
 mainFn = "main = print $ dePI $ veval (veval (veval (veval Cmain_0 (PI 0)) PSucc_0) PAdd_0) PMul_0"
 
