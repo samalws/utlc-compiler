e75aaa41b94d6fedf040e686119a9b418ae3d6ed
Sam Alws <sam@samalws.com>
2021-09-29 20:34:10 -0400

    we don't need to print stuff out anymore

--- Main.hs
+++ Main.hs
@@ -11,19 +11,13 @@ main = do
   mainFn      <- readFile "Additions/MainFn"
 
   text <- readFile "main.utlc"
-  putStrLn text
   let parsed = parse codeFileParser "main.utlc" text
   let frParsed = fromRight (Code0 []) parsed
   let converted = convCode2Hs typePostfix evalPostfix mainFn $ conv12Code $ conv01Code frParsed
   if (isRight parsed) then do
-    putStrLn converted
     writeFile "otp.hs" converted
-    putStrLn "outputted to otp.hs"
     callCommand "ghc -no-keep-hi-files -no-keep-o-files otp.hs"
-    putStrLn "built successfully"
     callCommand "time ./otp"
-    putStrLn "ran successfully"
     callCommand "rm otp.hs"
     callCommand "rm otp"
-    putStrLn "cleaned up successfully"
   else print parsed
